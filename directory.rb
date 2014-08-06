f6447a64200ada0a58e18f00fb5de15f10c547c3
Mervodactyl <merve.silk@gmaillcom>
2014-08-06 11:42:02 +0100

    nineth commit

--- directory.rb
+++ directory.rb
@@ -1,29 +1,29 @@
 #lets put all the students into an array
 students = [
-["Javier Silverio", :August],
-["Elliot Lewis", :August],
-["Ben Tillett", :August],
-["Vincent Koch", :August],
-["Michelle Ballard", :August],
-["Nick Roberts", :August],
-["Tatiana Soukiassian", :August],
-["Mervé Silk", :August],
-["Albert Vallverdu", :August],
-["Lovis Schultze", :August],
-["Henry Stanley", :August],
-["Spike Lindsey", :August],
-["Ruth Earle", :August],
-["Andy Gates", :August],
-["Faisal Aydarus", :August],
-["Ethel Ng", :August],
-["Kevin Daniells", :August],
-["Maya Driver", :August],
-["Leopold Kwok", :August],
-["James McNeil", :August],
-["Jerome Pratt", :August],
-["David Wickes", :August],
-["Chris Oatley", :August],
-["Marc Singh", :August],
+{:name => "Javier Silverio", :cohort =>  :August],
+{:name => "Elliot Lewis", :cohort =>  :August],
+{:name => "Ben Tillett", :cohort =>  :August],
+{:name => "Vincent Koch", :cohort =>  :August],
+{:name => "Michelle Ballard", :cohort =>  :August],
+{:name => "Nick Roberts", :cohort =>  :August],
+{:name => "Tatiana Soukiassian", :cohort =>  :August],
+{:name => "Mervé Silk", :cohort =>  :August],
+{:name => "Albert Vallverdu", :cohort =>  :August],
+{:name => "Lovis Schultze", :cohort =>  :August],
+{:name => "Henry Stanley", :cohort =>  :August],
+{:name => "Spike Lindsey", :cohort =>  :August],
+{:name => "Ruth Earle", :cohort =>  :August],
+{:name => "Andy Gates", :cohort =>  :August],
+{:name => "Faisal Aydarus", :cohort =>  :August],
+{:name => "Ethel Ng", :cohort =>  :August],
+{:name => "Kevin Daniells", :cohort =>  :August],
+{:name => "Maya Driver", :cohort =>  :August],
+{:name => "Leopold Kwok", :cohort =>  :August],
+{:name => "James McNeil", :cohort =>  :August],
+{:name => "Jerome Pratt", :cohort =>  :August],
+{:name => "David Wickes", :cohort =>  :August],
+{:name => "Chris Oatley", :cohort =>  :August],
+{:name => "Marc Singh", :cohort =>  :August],
 ]
 
 def print_header
@@ -33,7 +33,7 @@ end
 
 def print(students)
 	names.each do |student|
-		puts "#{student[0]} (#{student[1]} cohort)"
+		puts "#{student[:name]} (#{student[:cohort]} cohort)"
 	end
 end
 
