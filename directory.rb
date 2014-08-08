ea5b8758e4e1fc8cb03f18630b16a5ad5c185986
Mervodactyl <merve.silk@gmaillcom>
2014-08-08 17:58:19 +0100

    NoStudents:6.11

--- directory.rb
+++ directory.rb
@@ -1,61 +1,52 @@
 # lets put all the students into an array
  	MONTHS = %w(January Febuary March April May June July August September October November December) 
+@students = []
 
 def input_students
 	@students = []
-	more_student = "yes"
-	until more_student == "no" 
+	more_student = "Y"
+	until more_student == "N" 
 		puts "Please use the following instructions"
 		puts "Please enter the name of the new Student:"
-		name = get_information
+		name = gets.chomp
 			name = "I see you must be a secret agent" if name == ""
 		puts "Please enter cohort:"
-		cohort = get_cohort
+		cohort = gets.chomp
 			cohort = "I already know where you live" if cohort == ""
 		puts "Please enter the student's age:"
-		age = get_information
+		age = gets.chomp
 			age = "You must be embarassed of your wrinkly butt" if age == ""
 		puts "Please enter the student's country of origin:"
-		country = get_information
+		country = gets.chomp
 			country = "why do you never take me anywhere nice?" if country == ""
-		puts "Do you want to enter another student to the directory? yes or no"
-		more_student = get_information
-		add_student(name,age,cohort,country)
+		puts "Do you want to enter another student to the directory? Y or N"
+		more_student = gets.chomp.capitalize
+		add_student(name, age, cohort, country)
 	end
 	@students
 end
 
-def get_cohort
-	info_given = gets.chomp
-	until MONTHS.include? info_given.downcase.capitalize
-		puts "sorry that month doest exist, please enter it again"
-		info_given = gets.chomp
-	end
-	info_given	
-end
-
-def get_information
-	entered_value = gets.chomp
-	puts "You entered #{entered_value}, are you ok with that? Y or N"
-	ya_or_nay = gets.chomp
-	until ya_or_nay.downcase.start_with?("y")
-		puts "well enter it again then"
-		entered_value = gets.chomp
-		puts "You entered #{entered_value}, are you ok with that? Y or N"
-		ya_or_nay = gets.chomp
-	end
-	entered_value
-end
-	
-
 def add_student(name, age, cohort, country)
 		@students << {:name => name, :cohort => cohort, :age => age, :country => country}
-		puts "Now we have #{@students.length} students"
+		puts "Now we have #{@students.length} student#{'s' if @students.length > 1}"
 end
 
 def print_header
 	puts "The students of Makers Academy".center(70, '******')
 	puts "______________"
+end
+
+def cohorts(student)
+	student.map {|mapitem| mapitem[:cohort]}.uniq
+end
+
+def print_cohort(student)
+	cohorts.downcase.each_with_index do |cohort, index|
+		puts "cohort #{index + 1}. #{cohort}"
+		student.select {x | x[:cohort] == cohort}.each_with_index do |student, i|
+			puts "#{i + 1}. This is #{student[:name]}, their age is #{age [:age]}, their cohort is #{cohort [:cohort]} and they were born in #{country[:country]}."
+		end
+	end
 end
 
 def print_names(students)
@@ -69,15 +60,15 @@ def print_names(students)
 end
 
 def print_footer(names)
-	puts "Overall, we have #{names.length} great students"
+	puts "Now we have #{@students.length} great student#{'s' if @students.length > 1}".center(70, '******')
 end
 
 
 # nothing happens until we call the methods
 students = input_students
 print_header
-print_names (students)
-print_footer (students)
+print_names (@students)
+print_footer (@students)
 
 
 
